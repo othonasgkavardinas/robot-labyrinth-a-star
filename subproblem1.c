0cfcdc8e6ba2431cf24b89aea8b3721fd57b7ca9
othonasgkavardinas <oth-onmoon@hotmail.com>
2020-12-22 19:45:43 +0200

    changes from laptop

--- subproblem1.c
+++ subproblem1.c
@@ -4,10 +4,16 @@
 #include <math.h>
 #include <time.h>
 #include <memory.h>
+#include <stdarg.h>
 
 #define N     5
-#define p     0.7
+#define P     0.7
 #define MAX   100
+
+#define G1    2
+#define G2    3
+#define T     4
+#define PASSED    7
 
 #define IS_INT   1
 #define IS_FLOAT 0
@@ -40,10 +46,12 @@ void alloc_nodes(node **, node **);
 void init_start_node(node *, vector, float [][N]);
 void copy_node_from(node *, node *);
 void find_min_node(node *, int *, node [], int);
-void shift_open_right(int, node *, int);
-void realloc_node_array(node *, int, int *);
-void set_temp_parents_and_total_cost(vector *, node, float *);
-node get_node(node *, node *, int, int *, int *, node);
+void remove_min_node(int, node *, int);
+void realloc_node_array_if_needed(node *, int, int *);
+int equals_vector(vector, vector);
+void set_temp_parent_and_total_cost(vector *, node, float *);
+void set_int_array(int *, int, ...);
+void set_vector_array_from_pairs(vector *, int, ...);
 void delete_node(node *, int, int, node);
 void copy_vector_from(vector *, vector *);
 
@@ -56,69 +64,63 @@ float do_manhattan(vector, vector);
 void calculate_distances(float [][N], vector);
 
 float a_star(int matrix[][N], float dists[][N], vector s, vector fast, int *extensions) {
-    int open_size=0, open_size_max=N, open_position, open_flag;
-    int close_size=0, close_size_max=N, close_position, close_flag;
+    int open_size=0, open_size_max=N;
+    int close_size=0, close_size_max=N;
     int min_position;
     node *open, *close;
     node min_node, start_node, direction_node, found, old;
     float total_cost = 0;
     vector temp_parent;
     int i;
+    int conditions_size, conditions[conditions_size];
+    vector direction_vectors[conditions_size];
 
     alloc_nodes(&open, &close);
-    init_start_node(&start_node, s, dists);
-    copy_node_from(&(open[open_size++]), &start_node);
-    
+    init_start_node(&(open[open_size++]), s, dists);
+
     while (1) {
         if (open_size == 0)
             break;
         find_min_node(&min_node, &min_position, open, open_size--);
-        shift_open_right(min_position, open, open_size);
-        realloc_node_array(close, close_size, &close_size_max);
+        remove_min_node(min_position, open, open_size);
+        realloc_node_array_if_needed(close, close_size, &close_size_max);
         close[close_size++] = min_node;
-        if (min_node.v[0] == fast[0] && min_node.v[1] == fast[1]) {
+        if (equals_vector(min_node.v, fast)) {
             *extensions = close_size;
-            matrix[min_node.parent[0]][min_node.parent[1]] = 7;
-            set_temp_parents_and_total_cost(&temp_parent, min_node, &total_cost);
+            matrix[min_node.parent[0]][min_node.parent[1]] = PASSED;
+            set_temp_parent_and_total_cost(&temp_parent, min_node, &total_cost);
             while (1) {
                 for (i=0; i<close_size; i++)
-                    if (close[i].v[0] == temp_parent[0] && close[i].v[1] == temp_parent[1])
+                    if (equals_vector(close[i].v, temp_parent)) {
                         copy_node_from(&found, &(close[i]));
-                set_temp_parents_and_total_cost(&temp_parent, found, &total_cost);
-                if (temp_parent[0] == s[0] && temp_parent[1] == s[1])
+                        set_temp_parent_and_total_cost(&temp_parent, close[i], &total_cost);
+                        break;
+                    }
+                if (equals_vector(temp_parent, s))
                     break;
-                matrix[found.parent[0]][found.parent[1]] = 7;
+                matrix[found.parent[0]][found.parent[1]] = PASSED;
             }
             break;
         } else {
-            char conditions_size = 4;
-            char conditions[] = { min_node.v[0] != 0 && matrix[(min_node.v[0]) - 1][min_node.v[1]] != 1,
+            conditions_size = 4;
+            set_int_array(conditions, 4,
+                min_node.v[0] != 0 && matrix[(min_node.v[0]) - 1][min_node.v[1]] != 1,
                 min_node.v[0] != N && matrix[(min_node.v[0]) + 1][min_node.v[1]] != 1,
                 min_node.v[1] != 0 && matrix[min_node.v[0]][(min_node.v[1]) - 1] != 1,
-                min_node.v[1] != N && matrix[min_node.v[0]][(min_node.v[1]) + 1] != 1 };
-            int direction_node_new_i[] = { min_node.v[0]-1, min_node.v[0]+1, min_node.v[0], min_node.v[0] };
-            int direction_node_new_j[] = { min_node.v[1], min_node.v[1], min_node.v[1]-1, min_node.v[1]+1 };
-                
+                min_node.v[1] != N && matrix[min_node.v[0]][(min_node.v[1]) + 1] != 1);
+            set_vector_array_from_pairs(direction_vectors, 4,
+                min_node.v[0]-1, min_node.v[1],
+                min_node.v[0]+1, min_node.v[1],
+                min_node.v[0], min_node.v[1]-1,
+                min_node.v[0], min_node.v[1]+1);
+
             for(i=0; i<conditions_size; i++)
                 if(conditions[i]) {
-                    open_flag = 0;
-                    close_flag = 0;
-                    direction_node.v[0] = direction_node_new_i[i];
-                    direction_node.v[1] = direction_node_new_j[i];
+                    copy_vector_from(&(direction_node.v), &(direction_vectors[i]));
                     copy_vector_from(&(direction_node.parent), &(min_node.v));
                     direction_node.f = do_manhattan(s, direction_node.v) + dists[direction_node.v[0]][direction_node.v[1]];
-                    get_node(&old, open, open_size, &open_position, &open_flag, direction_node);
-                    get_node(&old, close, close_size, &close_position, &close_flag, direction_node);
-                    if (open_flag == 0 && close_flag == 0) {
-                        realloc_node_array(open, open_size, &open_size_max);
-                        copy_node_from(&(open[open_size++]), &direction_node);
-                    }
-                    else if (old.f > direction_node.f) {
-                        if (open_flag == 1)
-                            delete_node(open, open_size, open_position, direction_node);
-                        else if (close_flag == 1)
-                            delete_node(close, close_size, close_position, direction_node);
-                    }
+                    realloc_node_array_if_needed(open, open_size, &open_size_max);
+                    copy_node_from(&(open[open_size++]), &direction_node);
                 }
         }
     }
@@ -146,13 +148,10 @@ void copy_node_from(node *n1, node *n2) {
 }
 
 void find_min_node(node *min_node, int *min_position, node open[], int open_size) {
-    int min_f;
-    int i;
-
+    int i, min_f;
     min_f = open[0].f;
     *min_position = 0;
     copy_node_from(min_node, &(open[0]));
-
     if (open_size > 1)
         for (i=1; i<open_size; i++)
             if (open[i].f < min_f) {
@@ -162,29 +161,19 @@ void find_min_node(node *min_node, int *min_position, node open[], int open_size
             }
 }
 
-void shift_open_right(int min_position, node *open, int open_size) {
+void remove_min_node(int min_position, node *open, int open_size) {
     int i;
-    for (i=0; i<open_size; i++)
-        if (i>=min_position)
-            copy_node_from(&(open[i]), &(open[i+1]));
+    for (i=min_position; i<open_size-1; i++)
+        copy_node_from(&(open[i]), &(open[i+1]));
 }
 
-void realloc_node_array(node *array, int size, int *size_max) {
-    if(size+1 >= *size_max) {
-        *size_max += N;
-        array = (node *) realloc(array, (*size_max)*sizeof(node));
-    }
+void realloc_node_array_if_needed(node *array, int size, int *size_max) {
+    if(size+1 >= *size_max)
+        array = (node *) realloc(array, (*size_max+=N)*sizeof(node));
 }
 
-node get_node(node *old, node *array, int array_size, int *array_position, int *array_flag, node direction) {
-    int i;
-    for (i=0; i<array_size; i++)
-        if (array[i].v[0] == direction.v[0] && array[i].v[1] == direction.v[1]) {
-            copy_node_from(old, &(array[i]));
-            *array_position = i;
-            *array_flag = 1;
-            break;
-        }
+int equals_vector(vector v1, vector v2) {
+    return v1[0]==v2[0] && v1[1]==v2[1];
 }
 
 void delete_node(node *array, int array_size, int array_position, node direction) {
@@ -196,9 +185,28 @@ void delete_node(node *array, int array_size, int array_position, node direction
     copy_node_from(&(array[array_size++]), &direction);
 }
 
-void set_temp_parents_and_total_cost(vector *temp_parent, node n, float *total_cost) {
+void set_temp_parent_and_total_cost(vector *temp_parent, node n, float *total_cost) {
     copy_vector_from(temp_parent, &(n.parent));
     *total_cost += n.f;
+}
+
+void set_int_array(int *array, int size, ...) {
+    int i;
+    va_list args;
+    va_start(args, size);
+    for(i=0; i<size; i++)
+        array[i] = va_arg(args, int);
+    va_end(args);
+}
+
+void set_vector_array_from_pairs(vector *array, int size, ...) {
+    int i, j;
+    va_list args;
+    va_start(args, size);
+    for(i=0; i<size; i+=2)
+      for(j=0; j<2; j++)
+        array[i][j] = va_arg(args, int);
+    va_end(args);
 }
 
 void copy_vector_from(vector *v1, vector *v2) {
@@ -241,7 +249,7 @@ int main() {
     calculate_distances(distances, fast);
     print_matrix(distances, IS_FLOAT);
     cost1 = a_star(matrix, distances, s, fast, &extensions);
-    
+
     print_matrix(matrix, IS_INT);
     printf("cost1: %f\n", cost1);
     printf("first extensions: %d\n", extensions);
@@ -254,7 +262,7 @@ int main() {
     printf("cost2: %f\n", cost2);
     printf("total cost: %f\n", cost1 + cost2);
     printf("total extensions: %d\n", extensions);
-    
+
     return 0;
 }
 
@@ -264,7 +272,7 @@ void create_matrix_with_boundaries(int matrix[][N]) {
     for (i=0; i<N; i++)
         for (j=0; j<N; j++) {
             p_scaled = (int)((rand() % MAX) + 1);
-            p_scaled > p*((int)MAX+1) ? (matrix[i][j]=1) : (matrix[i][j]=0);
+            p_scaled > P*((int)MAX+1) ? (matrix[i][j]=1) : (matrix[i][j]=0);
         }
 }
 
@@ -295,9 +303,9 @@ void input_options(vector s, vector x, vector y) {
 }
 
 void set_options(int matrix[][N], vector s, vector x, vector y) {
-    matrix[s[0]][s[1]] = 2;
-    matrix[x[0]][x[1]] = 3;
-    matrix[y[0]][y[1]] = 4;
+    matrix[s[0]][s[1]] = G1;
+    matrix[x[0]][x[1]] = G2;
+    matrix[y[0]][y[1]] = T;
 }
 
 float do_manhattan(vector s, vector g) {
